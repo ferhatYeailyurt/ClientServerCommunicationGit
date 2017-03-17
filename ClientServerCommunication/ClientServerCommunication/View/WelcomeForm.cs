9e1846050c276c41529bcac5a42b290a6d3efe04
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-03-17 21:23:05 +0200

    v1
    create project - version1

---
+++ ClientServerCommunication/ClientServerCommunication/View/WelcomeForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ClientServerCommunication.View;
+
+namespace ClientServerCommunication
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WelcomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserRegister userRegister = new UserRegister();
+            userRegister.Show();
+            this.Hide();
+        }
+    }
+}
