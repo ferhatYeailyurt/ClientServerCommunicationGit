82d7cf1d075810d156fac446cd7fb649b8794bf3
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-03-18 11:35:02 +0200

    Model package and Controller package import

---
+++ ClientServerCommunication/ClientServerCommunication/Model/User_ModelCS.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerCommunication.Model
+{
+    class User_ModelCS
+    {
+        private String ad;
+        private String soyad;
+        private String email;
+        private String sifre;
+
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+
+            set
+            {
+                ad = value;
+            }
+        }
+
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+
+            set
+            {
+                soyad = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Sifre
+        {
+            get
+            {
+                return sifre;
+            }
+
+            set
+            {
+                sifre = value;
+            }
+        }
+    }
+}
